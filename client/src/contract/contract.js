ed5a3d768219148b9f0ac52f41a48ae9ce733f0d
datmedevil17 <rakshitshukla17@gmail.com>
2025-03-14 00:26:17 +0530

    code updated

---
+++ client/src/contract/contract.js
@@ -0,0 +1,17 @@
+import desciAbi from "./DeSciToken.json"
+import collabAbi from "./ResearchCollab.json"
+import companyAbi from "./ResearchCompany.json"
+import escrowAbi from "./ResearchEscrow.json"
+
+export const desciABI = desciAbi.abi;
+export const collabABI = collabAbi.abi;
+export const companyABI = companyAbi.abi;
+export const escrowABI = escrowAbi.abi;
+
+export const desciAddress = "0xb0470519c6011F0B676B09D2D807d71Ea44c2443"
+export const collabAddress = "0x0E58d6b79FFCe1141F9De389CA8A2c796F2e266e"
+export const companyAddress = "0xa9932205542cE9c18cB23ad17a63539Af95c5f41"
+export const escrowAddress = "0x63B721F5FC7f147279a4f1C995627eEa330BC426"
+
+
+
